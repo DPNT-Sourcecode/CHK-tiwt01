b852ffdf2e1b64344dac217a02d4068752baed24
sbx_user1051 <sbx_user1051@169.254.30.65>
2024-06-09 08:39:16 +0000

    Sun Jun 09 08:39:16 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,15 @@
+""" Diary
+* 2024-09-07 Sun
+** 0935
+
+On starting this morning there was an alert:
+
+  IMPORTANT! You are not allowed to work on the solution while the challenge is PAUSED.
+
+This wasn't anywhere in the documentation.  I did ~60 mins last night, pencil & paper sketches and refactoring.
+
+"""
+
 # +------+-------+----------------+
 # | Item | Price | Special offers |
 # +------+-------+----------------+
@@ -7,29 +19,41 @@
 # | D    | 15    |                |
 # +------+-------+----------------+
 
-INFO = {
-    'A': (50, (3, 130)),
-    'B': (30, (2, 45)),
-    'C': (20, None),
-    'D': (15, None),
+PRICES = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
     }
-LEGAL_SKUS = INFO.keys()
+LEGAL_SKUS = PRICES.keys()
+
+def multiple_price(basket, sku, context):
+    multi_amount = context['amount']
+    multi_price = context['price']
+    while basket.to_pay_for.get(sku, 0) >= multi_amount:
+        basket.to_pay_for[sku] -= multi_amount
+        basket.paid_for.setdefault(sku, 0)
+        basket.paid_for[sku] += multi_amount
+        basket.price += multi_price
+    return basket
+
+DISCOUNTS = {
+    'A': (multiple_price, {'amount': 3, 'price': 130}),
+    'B': (multiple_price, {'amount': 2, 'price': 45}),
+    }
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     if not valid(skus):
         return -1
-    d = parse_skus(skus)
-
-    result = 0
-    for item, amount in d.items():
-        price, offer = INFO[item]
-        if offer:
-            result += calc_offer(amount, price, offer)
-        else:
-            result += price * amount
-    return result
+    order = parse_skus(skus)
+    basket = Basket(order)
+    ds = applicable_discounts(basket)
+    for d in ds:
+        basket = apply_discount(d, basket)
+    basket.apply_vanilla_prices()
+    return basket.price
 
 def valid(skus):
     return all([i in LEGAL_SKUS for i in skus])
@@ -42,7 +66,24 @@ def parse_skus(skus):
                 d[i] += 1
     return d
     
-def calc_offer(amount, normal_price, offer):
-    multiple, special_price = offer
-    specials, normals = divmod(amount, multiple)
-    return (specials * special_price) + (normals * normal_price)
+def applicable_discounts(basket):
+    return [(sku, config) for (sku,config) in DISCOUNTS.items()
+            if sku in basket.to_pay_for.keys()
+            ]
+
+def apply_discount(discount, basket):
+    sku, config = discount
+    f, context = config
+    return f(basket, sku, context)
+
+class Basket:
+    def __init__(self, order):
+        self.price = 0
+        self.paid_for = dict()
+        self.to_pay_for = order
+
+    def apply_vanilla_prices(self):
+        for sku, amount in self.to_pay_for.items():
+            self.price += PRICES[sku] * amount
+            # self.to_pay_for.pop(sku) # TODO
+
